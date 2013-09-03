6c80f49133e0c6660e685d45d3472da2e2089639
jimmytien <jimmytien6222@hotmail.com>
2013-09-04 02:14:45 +0800

    1

--- StringParser/Form1.Designer.cs
+++ StringParser/Form1.Designer.cs
@@ -28,20 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 83);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(420, 188);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(473, 283);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- StringParser/Form1.cs
+++ StringParser/Form1.cs
@@ -7,14 +7,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace StringParser
 {
     public partial class Form1 : Form
     {
+        enum ParseCmd
+        {
+            ParseStart = 0,
+            ParseEnd   = 1,
+            ParseString = 3,
+        };
+        private List<string> cmd = new List<string>();
+        private string cmdString;
+        private string inString;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void parseString(string str)
+        {
+            bool pend = false;
+            int pos = 0;
+            int length = str.Length;
+            string tmp = "";
+            string cmd = "";
+            ParseCmd pCmd = ParseCmd.ParseString;
+
+
+
+
+
+            while(pos < length)
+            {
+                char c = str[pos++];
+                switch (c)
+                {
+                    case '[':
+                        pCmd = ParseCmd.ParseStart;
+                        continue;
+                        break;
+                    case ']':
+                        pCmd = ParseCmd.ParseEnd;
+                        continue;
+                        break;
+                    default:
+                        switch (pCmd)
+                        {
+                            case ParseCmd.ParseStart:
+                                cmd += c;
+                                break;
+                            case ParseCmd.ParseEnd:
+                                tmp += ParseCmd(cmd);
+                                break;
+                            case ParseCmd.ParseString:
+                                tmp += c;
+                                break;
+                        }
+                        break;
+                }
+            }
+
+        }
+        private string ParseCmd(string cmd)
+        {
+            string ostr = "";
+            switch (cmd[0])
+            {
+                case 'N':
+                    break;
+                    
+            }
+            return ostr;
         }
     }
 }
